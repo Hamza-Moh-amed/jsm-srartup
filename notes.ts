92738fe7acf0f092a245f66d638daddf9f0cf2f3
Hamza-Moh-amed <hamzamohamedworld@gmail.com>
2024-11-17 09:52:28 +0200

    uploadig to vercel

---
+++ notes.ts
@@ -0,0 +1,20 @@
+{
+  /*
+
+const posts = [
+    {
+      _createdAt: new Date(),
+      views: 5,
+      author: { _id: 5, name: "adrian" },
+      _id: 1,
+      description: "this is desc",
+      image: "/logo.png",
+      category: "Robots",
+      title: "we Robots",
+    },
+  ]
+
+
+
+*/
+}
